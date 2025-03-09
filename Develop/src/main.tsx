8f5e188a1e4e98be6a4de285e799dbdaecb78616
farmda31 <182030476+farmda31@users.noreply.github.com>
2025-03-09 16:58:04 -0500

    Add files via upload

---
+++ Develop/src/main.tsx
@@ -0,0 +1,38 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import React from 'react';
+import App from './App.tsx';
+import CandidateSearch from './pages/CandidateSearch.tsx';
+import SavedCandidates from './pages/SavedCandidates.tsx';
+import ErrorPage from './pages/ErrorPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <CandidateSearch />,
+      },
+      {
+        path: '/SavedCandidates',
+        element: <SavedCandidates />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
